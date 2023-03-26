0ffdb163145129c9a681d3b89795383edeaa4f9b
MrMoustacheTheCats75 <113124887+MrMoustacheTheCats75@users.noreply.github.com>
2023-03-27 01:49:15 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-export let urlBase = 'http://127.0.0.1:5500/';
-export let urlGetAll = 'http://localhost:8080/api/tasks/residents';
-export let urlGet = 'http://localhost:8080/api/tasks/resident/';
-export let urlAdd = 'http://localhost:8080/api/tasks/resident/add';
-export let urlUpdate = 'http://localhost:8080/api/tasks/resident/update/';
-export let urlDelete = 'http://localhost:8080/api/tasks/resident/delete/';
-
--- js/monday.js
+++
@@ -1,2 +0,0 @@
-import {One, Two, Three, taskWE, TaskTofT, TaskMandF, Week} from './obj.js';
-import {addResident, dellResident, updateResident, getRandomInt, choiceResident, choiceResidentH, createObjetResident} from './funct.js';
